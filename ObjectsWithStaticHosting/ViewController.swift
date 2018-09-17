c23b3befa0ddf8f075dbe19023d27bf6975106f7
timmybea <tim.beals@gmail.com>
2018-09-17 10:37:00 -0700

    basic project setup and gitignore

---
+++ ObjectsWithStaticHosting/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ObjectsWithStaticHosting
+//
+//  Created by Tim Beals on 2018-09-17.
+//  Copyright © 2018 Roobi Creative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
